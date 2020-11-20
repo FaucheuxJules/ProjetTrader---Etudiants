cd81f74211c0da9f6db622da4864f3802b06a382
Jules FAUCHEUX <jfaucheux@lerebours.org>
2020-11-20 10:58:29 +0100

    Ajoutez des fichiers projet.

---
+++ GestionnaireBDD/GstBdd.cs
@@ -0,0 +1,92 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using MetierTrader;
+using System.Collections.Generic;
+
+namespace GestionnaireBDD
+{
+    public class GstBdd
+    {
+        private MySqlConnection cnx;
+        private MySqlCommand cmd;
+        private MySqlDataReader dr;
+
+        // Constructeur
+        public GstBdd()
+        {
+            string chaine = "Server=localhost;Database=bourse;Uid=root;Pwd=";
+            cnx = new MySqlConnection(chaine);
+            cnx.Open();
+        }
+
+        public List<Trader> getAllTraders()
+        {
+            List<Trader> mesTraders = new List<Trader>();
+            cmd = new MySqlCommand("select idTrader, nomTrader from trader", cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Trader unTrader = new Trader(Convert.ToInt16(dr[0]), dr[1].ToString());
+                mesTraders.Add(unTrader);
+            }
+            dr.Close();
+            return mesTraders;
+        }
+        public List<ActionPerso> getAllActionsByTrader(int numTrader)
+        {
+            List<ActionPerso> mesActionPerso = new List<ActionPerso>();
+            cmd = new MySqlCommand("SELECT numAction, action.nomAction, prixAchat, quantite, SUM(prixAchat * quantite) AS total  FROM acheter inner join action on acheter.numAction = action.idAction WHERE acheter.numTrader =" + numTrader, cnx);
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                ActionPerso uneAction = new ActionPerso(Convert.ToInt16(dr[0]), dr[1].ToString(), Convert.ToDouble(dr[2]), Convert.ToInt16(dr[3]), Convert.ToInt32(dr[4]));
+                mesActionPerso.Add(uneAction);
+
+            }
+            dr.Close();
+            return mesActionPerso;
+        }
+        //public List<MetierTrader.Action> GetAllAction()
+        //{
+        //    List<MetierTrader.Action> mesAction = new List<MetierTrader.Action>();
+        //    cmd = new MySqlCommand("select idAction, nomAction, coursReel from action", cnx);
+        //    dr = cmd.ExecuteReader();
+        //    while (dr.Read())
+        //    {
+        //        MetierTrader.Action uneAction = new MetierTrader.Action(Convert.ToInt16(dr[0]), dr[1].ToString(), Convert.ToDouble(dr[2]));
+        //        mesAction.Add(uneAction);
+        //    }
+        //    dr.Close();
+        //    return mesAction;
+        //}
+
+        public List<MetierTrader.Action> getAllActionsNonPossedees(int numTrader)
+        {
+            return null;
+        }
+
+        public void SupprimerActionAcheter(int numAction, int numTrader)
+        {
+   
+            
+        }
+
+        public void UpdateQuantite(int numAction, int numTrader, int quantite)
+        {
+            
+        }
+
+        public double getCoursReel(int numAction)
+        {
+            return 0;
+        }
+        public void AcheterAction(int numAction, int numTrader, double prix, int quantite)
+        {
+
+        }
+        public double getTotalPortefeuille(int numTrader)
+        {
+            return 0;
+        }
+    }
+}
---
+++ MetierTrader/ActionPerso.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetierTrader
+{
+    public class ActionPerso
+    {
+        private int numAction;
+        private string nomAction;
+        private double prixAchat;
+        private int quantite;
+        private int total;
+
+
+        public ActionPerso (int unNumAction, string unNomAction, double unPrixAchat, int uneQuantite, int unTotal)
+        {
+            NumAction = unNumAction;
+            NomAction = unNomAction;
+            PrixAchat = unPrixAchat;
+            Quantite = uneQuantite;
+            Total = unTotal;
+
+        }
+
+        public int NumAction { get => numAction; set => numAction = value; }
+        public string NomAction { get => nomAction; set => nomAction = value; }
+        public double PrixAchat { get => prixAchat; set => prixAchat = value; }
+        public int Quantite { get => quantite; set => quantite = value; }
+        public int Total { get => total; set => total = value; }
+    }
+}
